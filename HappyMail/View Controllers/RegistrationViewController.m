de2c5d5171d5e706e7a8c4ed28ec345327607c41
Mercy Bickell <mercycat@fb.com>
2020-07-13 14:52:05 -0600

    set up registration view controller and registration

--- HappyMail/View Controllers/RegistrationViewController.m
+++ HappyMail/View Controllers/RegistrationViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RegistrationViewController.h"
+#import "User.h"
 
 @interface RegistrationViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -19,14 +22,40 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Actions
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)didPressRegister:(id)sender {
+    [self registerUser];
 }
-*/
+
+#pragma mark - Parse network calls
+
+- (void)registerUser {
+    // Initialize a user object
+    User *newUser = [User user];
+    
+//    // Check for empty username or password
+//    if ([self.usernameField.text isEqual:@""]) {
+//        [self createAlertWithMessage:@"Please enter your username" withTitle:@"Username required"];
+//    }
+//    if([self.passwordField.text isEqual:@""]) {
+//        [self createAlertWithMessage:@"Please enter your password" withTitle:@"Password required"];
+//    }
+    
+    // Set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // Call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+            //[self createAlertWithMessage:error.localizedDescription withTitle:@"Error signing up"];
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"registrationSegue" sender:nil];
+        }
+    }];
+}
 
 @end
