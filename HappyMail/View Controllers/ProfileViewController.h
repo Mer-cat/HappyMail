08a9894da1689e25743b98cf6a772ad5231c43bf
Mercy Bickell <mercycat@fb.com>
2020-07-13 14:00:17 -0600

    add initial navigational skeleton

---
+++ HappyMail/View Controllers/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/13/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
