86173977f447321236b43bcf5d35d2fc50e6f304
Mercy Bickell <mercycat@fb.com>
2020-07-13 15:29:33 -0600

    enable persisting user sessions

--- HappyMail/SceneDelegate.m
+++ HappyMail/SceneDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "User.h"
 
 @interface SceneDelegate ()
 
@@ -16,9 +17,15 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    // Enables persisting user sessions
+    User *user = [User currentUser];
+    if (user != nil) {
+        NSLog(@"Welcome back %@", user.username);
+        
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UIViewController *tabBarController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+        self.window.rootViewController = tabBarController;
+    }
 }
 
 
