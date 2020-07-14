fbe73d767660806595e5daae793493524a80440e
Mercy Bickell <mercycat@fb.com>
2020-07-14 13:17:32 -0600

    create custom cell for follow-ups

---
+++ HappyMail/Views/FollowUpCell.h
@@ -0,0 +1,20 @@
+//
+//  FollowUpCell.h
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/14/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Post.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FollowUpCell : UITableViewCell
+
+- (void)refreshFollowUp:(Post *)post;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HappyMail/Views/FollowUpCell.m
@@ -0,0 +1,21 @@
+//
+//  FollowUpCell.m
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/14/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "FollowUpCell.h"
+
+@interface FollowUpCell ()
+
+@end
+
+@implementation FollowUpCell
+
+- (void)refreshFollowUp:(Post *)post {
+    // Fill out depending on whether Post is a request or offer
+}
+
+@end
