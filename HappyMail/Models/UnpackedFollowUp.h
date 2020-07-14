91c065a5c4b20d49717099e6645f06467a2e1d63
Mercy Bickell <mercycat@fb.com>
2020-07-14 17:46:32 -0600

    add unpacked follow up model

--- HappyMail/Models/UnpackedFollowUp.h
+++ HappyMail/Models/UnpackedFollowUp.h
@@ -1,18 +1,18 @@
 //
-//  FollowUp.h
+//  UnpackedFollowUp.h
 //  HappyMail
 //
 //  Created by Mercy Bickell on 7/14/20.
 //  Copyright © 2020 mercycat. All rights reserved.
 //
 
-#import <Parse/Parse.h>
-#import "User.h"
+#import <Foundation/Foundation.h>
 #import "Post.h"
+#import "User.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FollowUp : PFObject<PFSubclassing>
+@interface UnpackedFollowUp : NSObject
 
 @property (nonatomic, strong) User *receivingUser;
 @property (nonatomic, strong) Post *originalPost;
