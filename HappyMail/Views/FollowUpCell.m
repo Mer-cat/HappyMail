385ed3902944c771b96bb4f79a333c406797d081
Mercy Bickell <mercycat@fb.com>
2020-07-14 14:01:39 -0600

    initialize follow up cell with information

--- HappyMail/Views/FollowUpCell.m
+++ HappyMail/Views/FollowUpCell.m
@@ -9,13 +9,21 @@
 #import "FollowUpCell.h"
 
 @interface FollowUpCell ()
+@property (weak, nonatomic) IBOutlet UILabel *postTypeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *postTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
 
 @end
 
 @implementation FollowUpCell
 
 - (void)refreshFollowUp:(FollowUp *)followUp {
-  
+    NSString *postType = _PostTypes()[followUp.originalPost.type];
+    self.postTypeLabel.text = [NSString stringWithFormat:@"%@:", postType];
+    self.postTitleLabel.text = followUp.originalPost.title;
+    self.usernameLabel.text = followUp.receivingUser.username;
+    self.addressLabel.text = followUp.receivingUser.address;
 }
 
 @end
