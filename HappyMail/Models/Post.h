015750f57d82f19cdfc951bc1b1b55cfb5b69b86
Mercy Bickell <mercycat@fb.com>
2020-07-15 11:00:21 -0600

    make method for adding respondee to responders

--- HappyMail/Models/Post.h
+++ HappyMail/Models/Post.h
@@ -41,6 +41,7 @@ static NSArray<NSString *> *_PostTypes() {
 // MARK: Methods
 + (void)createNewPostWithTitle:(NSString * _Nullable)title withBody:(NSString * _Nullable)bodyText withType:(NSInteger)type withCompletion:(PFBooleanResultBlock  _Nullable)completion;
 
+- (void)addRespondee:(User *)user;
 - (void)removeRespondee:(User *)user;
 
 @end
--- HappyMail/Models/Post.m
+++ HappyMail/Models/Post.m
@@ -37,9 +37,26 @@
     [newPost saveInBackgroundWithBlock:completion];
 }
 
+- (void)addRespondee:(User *)user {
+    [self addObject:user forKey:@"respondees"];
+    [self saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            NSLog(@"%@ added to respondees for this post", user.username);
+        } else {
+            NSLog(@"Error adding user to respondees: %@", error.localizedDescription);
+        }
+    }];
+}
+
 - (void)removeRespondee:(User *)user {
     [self removeObject:user forKey:@"respondees"];
-    [self saveInBackground];
+    [self saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            NSLog(@"%@ removed from respondees for this post", user.username);
+        } else {
+            NSLog(@"Error removing user from respondees: %@", error.localizedDescription);
+        }
+    }];
 }
 
 @end
