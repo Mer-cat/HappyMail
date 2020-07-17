1d39e457373f091940a4123adc8fad783e709cc9
Mercy Bickell <mercycat@fb.com>
2020-07-17 13:52:41 -0600

    add search bar

--- HappyMail/View Controllers/FeedViewController.m
+++ HappyMail/View Controllers/FeedViewController.m
@@ -14,11 +14,13 @@
 #import "ProfileViewController.h"
 #import "ComposeViewController.h"
 
-@interface FeedViewController () <UITableViewDelegate, UITableViewDataSource, ComposeViewControllerDelegate>
+@interface FeedViewController () <UITableViewDelegate, UITableViewDataSource, ComposeViewControllerDelegate, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *posts;
+@property (nonatomic, strong) NSArray *filteredPosts;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
 
@@ -31,6 +33,7 @@
     
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.searchBar.delegate = self;
     
     [self fetchPosts];
     
@@ -54,6 +57,7 @@
     [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> *posts, NSError *error) {
         if (posts != nil) {
             self.posts = (NSMutableArray *) posts;
+            self.filteredPosts = posts;
             [self.tableView reloadData];
         } else {
             NSLog(@"Error getting posts: %@", error.localizedDescription);
@@ -67,7 +71,6 @@
 - (void)didPost:(Post *) post {
     [self.posts insertObject:post atIndex:0];
     [self.tableView reloadData];
-    NSLog(@"just reloaded due to new post");
 }
 
 #pragma mark - UITableViewDataSource
@@ -75,7 +78,7 @@
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     
     PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
-    Post *post = self.posts[indexPath.row];
+    Post *post = self.filteredPosts[indexPath.row];
     
     // Load the cell with current post
     [cell refreshPost:post];
@@ -83,7 +86,43 @@
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.posts.count;
+    return self.filteredPosts.count;
+}
+
+#pragma mark - UISearchBarDelegate
+
+// Filters movies shown based on search query
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    
+    if(searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(Post *evaluatedObject, NSDictionary *bindings) {
+            return [evaluatedObject.title containsString:searchText];
+        }];
+        self.filteredPosts = [self.posts filteredArrayUsingPredicate:predicate];
+    }
+    else {
+        self.filteredPosts = self.posts;
+    }
+    
+    [self.tableView reloadData];
+}
+
+-(void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = YES;
+}
+
+// Hides the search bar and clears it if user cancels
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = NO;
+    self.searchBar.text = @"";
+    [self.searchBar resignFirstResponder];
+    self.filteredPosts = self.posts;
+    [self.tableView reloadData];
+}
+
+// Hides the search bar when search complete
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self.searchBar resignFirstResponder];
 }
 
 #pragma mark - Navigation
