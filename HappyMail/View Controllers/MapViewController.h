183dacc37d0cdd9bfb57e17a410c10562f035fd0
Mercy Bickell <mercycat@fb.com>
2020-07-21 11:57:24 -0600

    add view controller to handle map view

---
+++ HappyMail/View Controllers/MapViewController.h
@@ -0,0 +1,18 @@
+//
+//  MapViewController.h
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/21/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HappyMail/View Controllers/MapViewController.m
@@ -0,0 +1,40 @@
+//
+//  MapViewController.m
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/21/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface MapViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.mapView.delegate = self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)initMapWithUser:(User *)user {
+    // Load in list of coordinates from user
+    // Populate map with pins for each coordinate
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
