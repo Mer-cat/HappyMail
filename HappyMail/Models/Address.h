e8521444a2f705579c3c10171ba2bb1ae61f474c
Mercy Bickell <mercycat@fb.com>
2020-07-21 11:56:58 -0600

    add address data model with creation method

---
+++ HappyMail/Models/Address.h
@@ -0,0 +1,25 @@
+//
+//  Address.h
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/21/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Address : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSString *streetAddress;
+@property (nonatomic, strong) NSString *city;
+@property (nonatomic, strong) NSString *state;
+@property (nonatomic, strong) NSString *zipcode;
+
+
++ (void)createNewAddress:(NSString *)streetAddress city:(NSString *)city state:(NSString *)state zipcode:(NSString *)zipcode withCompletion:(void (^)(Address *, NSError *))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HappyMail/Models/Address.m
@@ -0,0 +1,39 @@
+//
+//  Address.m
+//  HappyMail
+//
+//  Created by Mercy Bickell on 7/21/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "Address.h"
+
+@implementation Address
+
+@dynamic streetAddress;
+@dynamic city;
+@dynamic state;
+@dynamic zipcode;
+
++ (nonnull NSString *)parseClassName {
+    return @"Address";
+}
+
++ (void)createNewAddress:(NSString *)streetAddress city:(NSString *)city state:(NSString *)state zipcode:(NSString *)zipcode withCompletion:(void (^)(Address *, NSError *))completion {
+    
+    Address *newAddress = [Address new];
+    newAddress.streetAddress = streetAddress;
+    newAddress.city = city;
+    newAddress.state = state;
+    newAddress.zipcode = zipcode;
+    
+    [newAddress saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            completion(newAddress, nil);
+        } else {
+            completion(nil, error);
+        }
+    }];
+}
+
+@end
