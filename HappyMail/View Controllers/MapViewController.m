b35581cef6767b75da7b1d80ec7dd3189b1b98a2
Mercy Bickell <mercycat@fb.com>
2020-07-21 17:06:09 -0600

    add init function to show pins on map

--- HappyMail/View Controllers/MapViewController.m
+++ HappyMail/View Controllers/MapViewController.m
@@ -9,8 +9,12 @@
 #import "MapViewController.h"
 #import <MapKit/MapKit.h>
 
+
 @interface MapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) User *user;
+@property (nonatomic, strong) NSArray *sentToUsers;
+@property (nonatomic, strong) NSMutableArray *placesSentTo;
 
 @end
 
@@ -19,12 +23,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.mapView.delegate = self;
-    // Do any additional setup after loading the view.
+    self.user = [User currentUser];
+    
+    [self initMap];
 }
 
-- (void)initMapWithUser:(User *)user {
+- (void)initMap {
     // Load in list of coordinates from user
     // Populate map with pins for each coordinate
+    self.sentToUsers = self.user.sentToUsers;
+    for (User *user in self.sentToUsers) {
+        [user fetchIfNeededInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
+            if (object) {
+                [self.placesSentTo addObject:user.address];
+            } else {
+                NSLog(@"Error with fetching user address: %@", error.localizedDescription);
+            }
+        }];
+    }
+
+    for (Address *address in self.placesSentTo) {
+        // Drop pin on map for each address.coordinate
+        MKPointAnnotation *annotation = [MKPointAnnotation new];
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(address.latitude.floatValue, address.longitude.floatValue);
+        annotation.coordinate = coordinate;
+        annotation.title = @"You sent a card here!";
+        [self.mapView addAnnotation:annotation];
+    }
 }
 
 /*
