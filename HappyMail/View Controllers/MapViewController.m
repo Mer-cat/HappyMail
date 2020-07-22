36b4c1cc5a157731941c48662e853088174f06ce
Mercy Bickell <mercycat@fb.com>
2020-07-22 10:05:41 -0600

    map shows pins for each place user has sent a card

--- HappyMail/View Controllers/MapViewController.m
+++ HappyMail/View Controllers/MapViewController.m
@@ -8,6 +8,7 @@
 
 #import "MapViewController.h"
 #import <MapKit/MapKit.h>
+#import "Address.h"
 
 
 @interface MapViewController () <MKMapViewDelegate>
@@ -20,6 +21,8 @@
 
 @implementation MapViewController
 
+#pragma mark - View lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.mapView.delegate = self;
@@ -28,29 +31,63 @@
     [self initMap];
 }
 
+#pragma mark - Init
+
 - (void)initMap {
     // Load in list of coordinates from user
     // Populate map with pins for each coordinate
     self.sentToUsers = self.user.sentToUsers;
+    [self initPlacesSentTo];
+}
+
+- (void)initPlacesSentTo {
     for (User *user in self.sentToUsers) {
+        NSLog(@"Found a user");
         [user fetchIfNeededInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
             if (object) {
+                NSLog(@"%@", user.address);
                 [self.placesSentTo addObject:user.address];
+                [self dropPin:user.address];
             } else {
                 NSLog(@"Error with fetching user address: %@", error.localizedDescription);
             }
         }];
     }
-
-    for (Address *address in self.placesSentTo) {
-        // Drop pin on map for each address.coordinate
-        MKPointAnnotation *annotation = [MKPointAnnotation new];
-        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(address.latitude.floatValue, address.longitude.floatValue);
-        annotation.coordinate = coordinate;
-        annotation.title = @"You sent a card here!";
-        [self.mapView addAnnotation:annotation];
-    }
 }
+
+#pragma mark - Helper
+
+- (void)dropPin:(Address *)address {
+    // Drop pin on map for each address.coordinate
+    [address fetchIfNeededInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
+        if (object) {
+            NSLog(@"Dropped pin");
+            MKPointAnnotation *annotation = [MKPointAnnotation new];
+            CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(address.latitude.floatValue, address.longitude.floatValue);
+            annotation.coordinate = coordinate;
+            annotation.title = @"You sent a card here!";
+            [self.mapView addAnnotation:annotation];
+        } else {
+            NSLog(@"Failed to drop pin: %@", error.localizedDescription);
+        }
+    }];
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        //annotationView.canShowCallout = true;
+        //annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)]
+        annotationView.pinTintColor = [MKPinAnnotationView purplePinColor];
+        annotationView.animatesDrop = YES;
+    }
+    
+    return annotationView;
+}
+
 
 /*
 #pragma mark - Navigation
