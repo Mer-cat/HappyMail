2b9396e55ec5f394f9a95a947e0dfa365cbaa064
Mercy Bickell <mercycat@fb.com>
2020-08-05 15:08:35 -0600

    store mentions and hide tag field when post is not a thanks

--- HappyMail/View Controllers/ComposeViewController.m
+++ HappyMail/View Controllers/ComposeViewController.m
@@ -12,6 +12,7 @@
 #import "HKWMentionsPlugin.h"
 #import "HKWMentionsPluginV1.h"
 #import "MentionsManager.h"
+#import "HKWMentionsAttribute.h"
 
 
 @interface ComposeViewController () <HKWTextViewDelegate>
@@ -25,6 +26,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *limitQuestionLabel;
 @property (weak, nonatomic) IBOutlet HKWTextView *userTagTextView;
 @property (nonatomic, strong) id<HKWMentionsPlugin> plugin;
+@property (weak, nonatomic) IBOutlet UILabel *tagInstructionsLabel;
 
 @end
 
@@ -35,10 +37,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initMentionsPlugin];
+    [self hideThankYouFields];
     
     // Round corners
     [Utils roundCorners:self.responseLimitLabel];
     [Utils roundCorners:self.bodyTextView];
+    [Utils roundCorners: self.userTagTextView];
 }
 
 #pragma mark - Init
@@ -46,13 +50,12 @@
 - (void)initMentionsPlugin {
     self.userTagTextView.externalDelegate = self;
     
-    NSCharacterSet *controlCharacters = [NSCharacterSet characterSetWithCharactersInString:@"@+"];
-    id<HKWMentionsPlugin> mentionsPlugin = [HKWMentionsPluginV1 mentionsPluginWithChooserMode:HKWMentionsChooserPositionModeEnclosedTop controlCharacters:controlCharacters searchLength:0];
+    NSCharacterSet *controlCharacters = [NSCharacterSet characterSetWithCharactersInString:@"@"];
+    id<HKWMentionsPlugin> mentionsPlugin = [HKWMentionsPluginV1 mentionsPluginWithChooserMode:HKWMentionsChooserPositionModeEnclosedTop controlCharacters:controlCharacters searchLength:2];
     self.plugin = mentionsPlugin;
     
     mentionsPlugin.defaultChooserViewDelegate = [MentionsManager sharedInstance];
     mentionsPlugin.stateChangeDelegate = [MentionsManager sharedInstance];
-    mentionsPlugin.resumeMentionsCreationEnabled = YES;
     
     self.userTagTextView.controlFlowPlugin = mentionsPlugin;
 }
@@ -71,7 +74,13 @@
     NSInteger responseLimit = (int) self.responseLimitStepper.value;
     
     // Should only apply for Thank You posts
-    NSArray *taggedUsers = [[NSArray alloc] init];
+    
+    NSArray *taggedUsersAttributes = self.plugin.mentions;
+    NSMutableArray *taggedUsers = [[NSMutableArray alloc] init];
+    for (HKWMentionsAttribute *mention in taggedUsersAttributes) {
+        NSString *username = mention.mentionText;
+        [taggedUsers addObject:username];
+    }
     
     [Post createNewPostWithTitle:title withBody:bodyText withType:postType withLimit:responseLimit withTaggedUsers:taggedUsers withCompletion:^(Post *post, NSError *error) {
         if (post) {
@@ -88,17 +97,21 @@
 }
 
 - (IBAction)postTypeChanged:(id)sender {
-    if (self.postTypeControl.selectedSegmentIndex != Offer) {
-        self.responseLimitStepper.hidden = YES;
-        self.limitQuestionLabel.hidden = YES;
-        self.responseLimitLabel.hidden = YES;
-    } else {
-        self.responseLimitStepper.hidden = NO;
-        self.limitQuestionLabel.hidden = NO;
-        self.responseLimitLabel.hidden = NO;
-    }
-    if (self.postTypeControl.selectedSegmentIndex == ThankYou) {
-        // Enable user tagging
+    switch (self.postTypeControl.selectedSegmentIndex) {
+        case Offer:
+            [self showOfferFields];
+            [self hideThankYouFields];
+            break;
+        case Request:
+            [self hideOfferFields];
+            [self hideThankYouFields];
+            break;
+        case ThankYou:
+            [self hideOfferFields];
+            [self showThankYouFields];
+            break;
+        default:
+            break;
     }
 }
 
@@ -112,6 +125,30 @@
 - (IBAction)didPressCancel:(id)sender {
     // Return to the home screen
     [self dismissViewControllerAnimated:true completion:nil];
+}
+
+#pragma mark - Helpers
+
+- (void)showOfferFields {
+    self.responseLimitStepper.hidden = NO;
+    self.limitQuestionLabel.hidden = NO;
+    self.responseLimitLabel.hidden = NO;
+}
+
+- (void)hideOfferFields {
+    self.responseLimitStepper.hidden = YES;
+    self.limitQuestionLabel.hidden = YES;
+    self.responseLimitLabel.hidden = YES;
+}
+
+- (void)showThankYouFields {
+    self.tagInstructionsLabel.hidden = NO;
+    self.userTagTextView.hidden = NO;
+}
+
+- (void)hideThankYouFields {
+    self.tagInstructionsLabel.hidden = YES;
+    self.userTagTextView.hidden = YES;
 }
 
 @end
